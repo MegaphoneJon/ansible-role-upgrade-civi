304fccce9758233509103907cdd6b7596f3d222f
Jon Goldberg <jon@megaphonetech.com>
2023-09-27 10:59:41 -0400

    support updating extensions not approved for automatic distribution

--- helpers/upgradeable-civi.php
+++ helpers/upgradeable-civi.php
@@ -2,6 +2,24 @@
 <?php
 eval(`cv --level=cms-full php:boot`);
 
+/**
+ * We want to inject our own URL so we can see extensions that aren't approved for automated distribution.
+ */
+class myCRM_Extension_System extends CRM_Extension_System {
+
+  public function getBrowser() {
+    if ($this->browser === NULL) {
+      $url = CRM_Utils_System::evalUrl('https://civicrm.org/extdir/ver={ver}|cms={uf}|ready=');
+      $this->browser = new CRM_Extension_Browser($url, '');
+    }
+    return $this->browser;
+  }
+
+}
+
+$ces = new myCRM_Extension_System();
+$ces->getBrowser();
+CRM_Extension_System::setSingleton($ces);
 $e = new CRM_Admin_Page_Extensions();
 $localExtensionList = $e->formatLocalExtensionRows();
 $remoteExtensionList = $e->formatRemoteExtensionRows($localExtensionList);
